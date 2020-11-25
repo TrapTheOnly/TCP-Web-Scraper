376a340f83abb0f38602dfafb35f4e40006c157b
Ismail Eyyub <ismail.eyyub@gmail.com>
2020-11-25 17:58:08 +0400

    v2.1

--- web-scraper.py
+++ web-scraper.py
@@ -1,11 +1,14 @@
 import sys
 from bs4 import BeautifulSoup
+from termcolor import colored
 import socket
 import requests
 import argparse
+import threading
 
 HOST = "127.0.0.1"
 PORT = 65432
+ENCODING = 'utf-8'
 
 
 class Server:
@@ -16,34 +19,39 @@ class Server:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind((HOST, PORT))
         sock.listen(1)
-        print("[SERVER] has started")
+        print(
+            f"{colored('[SERVER]', 'green')} {colored('has started on ', 'yellow')}" +
+            f"{colored(str(HOST) + ':' + str(PORT), 'blue')}")
         while True:
-            sc, sockname = sock.accept()
-            url = (sc.recv(128)).decode('utf-8')
-            try:
-                content = requests.get("https://" + url).content
-                soup = BeautifulSoup(content, "html.parser")
-            except:
-                try:
-                    content = requests.get("http://" + url).content
-                    soup = BeautifulSoup(content, "html.parser")
-                except:
-                    content = requests.get(url).content
-                    soup = BeautifulSoup(content, "html.parser")
+            sc, addr = sock.accept()
+            thread = threading.Thread(target=self.handler, args=(sc, addr,))
+            thread.start()
+
+    def handler(self, sc, addr):
+        try:
+            fullUrl = (sc.recv(128)).decode(ENCODING)
+            url = 'https://' + \
+                fullUrl.split('//')[len(fullUrl.split('//')) - 1]
+            print(
+                f"{colored('Connected to', 'yellow')} {colored(str(addr[0]) + ':' + str(addr[1]), 'blue')} " +
+                f"{colored('and processing', 'yellow')} {colored(url, 'red')}")
+            content = requests.get(url).content
+            soup = BeautifulSoup(content, "html.parser")
             soup.prettify()
             pList = soup.find_all("p")
             leafCounter = 0
             for pElement in pList:
-                children = pElement.findChildren()
-                miniCounter = 0
-                for child in children:
-                    if child.name == "p":
-                        miniCounter += 1
-                if miniCounter == 0:
+                if not pElement.find_all("p"):
                     leafCounter += 1
             imageCounter = len(soup.find_all("img"))
-            finalResult = str(imageCounter) + " " + str(leafCounter)
-            sc.send(finalResult.encode('utf-8'))
+            finalResult = str(imageCounter) + ',' + \
+                str(leafCounter) + ',' + str(url)
+            sc.send(finalResult.encode(ENCODING))
+        finally:
+            sc.close()
+            print(
+                colored(f"Disconnected from {colored(str(addr[0]) + ':' + str(addr[1]), 'blue')}", 'red'))
+            print()
 
 
 class Client:
@@ -54,26 +62,30 @@ class Client:
     def start(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((HOST, PORT))
-        sock.send(self.url.encode('utf-8'))
+        sock.send(self.url.encode(ENCODING))
         data = sock.recv(128)
-        print(data.decode('utf-8'))
+        imgCount, leafCount, url = data.decode(ENCODING).split(',')
+        print(
+            f"{colored(url, 'red')} {colored('has', 'yellow')} " +
+            f"{colored(imgCount + ' images', 'green')} " +
+            f"{colored('and', 'yellow')} " +
+            f"{colored(leafCount + ' leaf paragraphs', 'blue')}")
 
 
 def main():
     parser = argparse.ArgumentParser(description='Send URL to the server')
-    parser.add_argument('server', nargs='?', help='Start as server')
-    parser.add_argument('client', nargs='?', help='Start as client')
+    parser.add_argument('mode', choices=('server', 'client'),
+                        help='Mode of running terminal')
     if 'client' in sys.argv:
         parser.add_argument('-p', type=str, help='URL of website')
     args = parser.parse_args()
-    if 'server' in sys.argv and 'client' in sys.argv:
-        parser.error("Use only one of [server, client]")
-    elif 'server' in sys.argv:
+    if args.mode == 'server':
         serverObj = Server()
-    elif 'client' in sys.argv:
+    elif args.mode == 'client':
         clientObj = Client(args.p)
     else:
-        parser.error("Enter one argument of [server, client]")
+        parser.error(
+            colored(f"Enter one argument of {colored('{server, client}', 'blue')}", "red"))
 
 
 if __name__ == "__main__":
